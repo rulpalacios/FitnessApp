d6dfd2071ad7cbb2cd6fa34e0f593c58a66f75ec
Raúl Palacios <raul@creatinkers.com>
2019-04-26 00:11:40 -0500

    JSX

--- src/index.js
+++ src/index.js
@@ -1,12 +1,38 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// const element = document.createElement('h1')
+// element.innerText = 'Hello React..'
+// const container = document.getElementById('root')
+// container.appendChild(element)
+// https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png
+import React from 'react'
+import ReactDOM from 'react-dom'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const user = {
+    firstName: 'Raul',
+    lastName: 'Palacios',
+    avatar: 'https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png'
+}
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+function getName(user){
+    return `${user.firstName} ${user.lastName}`
+}
+
+function getGreeting(user){
+    if(user){
+        return <h1>Hello {getName(user)} </h1>
+    }
+    return <h1>Hello Stranger </h1>
+}
+
+const name = 'Raul'
+
+const element = (
+    <div>
+        <h1>{getGreeting(user)}</h1>
+        <img src={user.avatar} />
+    </div>
+)
+const container = document.getElementById('root')
+
+// ReactDOM.render(__QUE__, __DONDE__)
+
+ReactDOM.render(element, container)
