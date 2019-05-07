2d6888ad761c3e44fe146183302ae296f58e8613
Raúl Palacios <raul@creatinkers.com>
2019-05-06 21:41:15 -0500

    styles

--- src/index.js
+++ src/index.js
@@ -6,6 +6,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
+import 'bootstrap/dist/css/bootstrap.css'
 
 const container = document.getElementById('root')
 
