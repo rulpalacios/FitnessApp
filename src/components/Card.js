75bd1e0a2a1cb67f63d0c5c19935a2d5dda6655f
Raúl Palacios <raul@creatinkers.com>
2019-05-09 11:12:31 -0500

    props

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,20 +1,26 @@
 import React from 'react'
 import exerciseImg from '../images/exercise.png'
+import circlesImg from '../images/circles.png'
 import './styles/Card.css'
 
 class Card extends React.Component {
-    
+
     render(){
+        const { title, description, img, leftColor, rightColor } = this.props
         return (
-            <div className="card mx-auto Fitness-Card">
+            <div className="card mx-auto Fitness-Card"
+            style={{
+                backgroundImage: `url(${circlesImg}), linear-gradient(to right, ${leftColor}, ${rightColor})`
+            }}
+            >
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={exerciseImg} className="float-right"/>
+                            <img src={img} className="float-right"/>
                         </div> 
                         <div className="col-6 Fitness-Card-Info">
-                            <h1>Technique Guides</h1>
-                            <p>Learn amazing street workout and calisthenics</p>
+                            <h1>{title}</h1>
+                            <p>{description}</p>
                         </div>
                     </div>
                 </div>
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@
 // const container = document.getElementById('root')
 // container.appendChild(element)
 // https://cdn4.iconfinder.com/data/icons/avatars-xmas-giveaway/128/batman_hero_avatar_comics-256.png
+//https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06
 import React from 'react'
 import ReactDOM from 'react-dom'
 import Card from './components/Card'
@@ -11,4 +12,10 @@ import 'bootstrap/dist/css/bootstrap.css'
 const container = document.getElementById('root')
 
 // ReactDOM.render(__QUE__, __DONDE__)
-ReactDOM.render(<Card />, container)
+ReactDOM.render(<Card 
+                    title="Technique Guides"
+                    description="Learn amazing street workout and calisthenics"
+                    img="https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06"
+                    leftColor="#A74CF2"
+                    rightColor="#617BFB"
+                />, container)
