1dee376522a6ef1ee40aa58f3e77e7392619d595
Raúl Palacios <raul@creatinkers.com>
2019-05-09 11:14:58 -0500

    state

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,9 +1,23 @@
 import React from 'react'
-import exerciseImg from '../images/exercise.png'
 import circlesImg from '../images/circles.png'
 import './styles/Card.css'
 
 class Card extends React.Component {
+
+    constructor(props){
+        super(props)
+        this.state = {
+            image: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/bulbasaur.png?alt=media&token=567caf19-af47-414e-a9d4-3854ab24c7dc'
+        }
+    }
+
+    componentDidMount(){
+        setTimeout(() => {
+            this.setState({
+                image: 'https://firebasestorage.googleapis.com/v0/b/tutoriales-e4830.appspot.com/o/exercise.png?alt=media&token=b9c4b236-16a9-4a56-bba2-90c9660a0f06'
+            })
+        }, 5000)
+    }
 
     render(){
         const { title, description, img, leftColor, rightColor } = this.props
@@ -16,7 +30,7 @@ class Card extends React.Component {
                 <div className="card-body">
                     <div className="row center">
                         <div className="col-6">
-                            <img src={img} className="float-right"/>
+                            <img src={this.state.image} className="float-right" alt="exercise"/>
                         </div> 
                         <div className="col-6 Fitness-Card-Info">
                             <h1>{title}</h1>
