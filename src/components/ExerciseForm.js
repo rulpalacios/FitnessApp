627a2120599a6e222e1dd94149f490a0161a7857
Raúl Palacios <raul@creatinkers.com>
2019-06-02 19:57:11 -0500

    Lifting State Up

---
+++ src/components/ExerciseForm.js
@@ -0,0 +1,83 @@
+import React from 'react'
+
+class ExerciseForm extends React.Component {
+
+    handleSubmit = e => {
+        e.preventDefault()
+        console.log(this.state)
+    }
+
+    render(){
+        const { onChange, form } = this.props
+
+        return (
+            <div className="container">
+            <form 
+                onSubmit={this.handleSubmit}
+            >
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="title" 
+                        name="title"
+                        onChange={onChange}
+                        value={form.title}
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="description" 
+                        name="description"
+                        onChange={onChange}
+                        value={form.description}
+                    />
+                </div>
+                <div className="form-group">
+                    <input 
+                        type="text" 
+                        className="form-control" 
+                        placeholder="img" 
+                        name="img"
+                        onChange={onChange}
+                        value={form.img}
+                    />
+                </div>
+                <div className="form-row">
+                    <div className="col">
+                        <input 
+                            type="text" 
+                            className="form-control" 
+                            placeholder="leftColor" 
+                            name="leftColor"
+                            onChange={onChange}
+                            value={form.leftColor}
+                        />
+                    </div>
+                    <div className="col">
+                        <input 
+                            type="text" 
+                            className="form-control"
+                            placeholder="rightColor" 
+                            name="rightColor"
+                            onChange={onChange}
+                            value={form.rightColor}
+                        />    
+                    </div>
+                </div>
+                
+                <button 
+                    type="submit" 
+                    className="btn btn-primary"
+                >
+                    Submit
+                </button>
+            </form>
+        </div>
+        )
+    }
+}
+
+export default ExerciseForm
--- src/pages/ExerciseNew.js
+++ src/pages/ExerciseNew.js
@@ -1,87 +1,41 @@
 import React from 'react'
+import ExerciseForm from '../components/ExerciseForm'
+import Card from '../components/Card'
 
 class ExerciseNew extends React.Component {
 
-    state = {}
-
-    handleSubmit = e => {
-        e.preventDefault()
-        console.log(this.state)
+    state = {
+        form: {
+            title: '',
+            description: '',
+            img: '',
+            leftColor: '',
+            rightColor: ''
+        }
     }
 
     handleChange = e => {
         this.setState({
-            [e.target.name]: e.target.value
+            form: {
+                ...this.state.form,
+                [e.target.name]: e.target.value
+            }
         })
     }
 
     render(){
         return (
-            <div className="container">
-            <form 
-                onSubmit={this.handleSubmit}
-            >
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="title" 
-                        name="title"
+            <div className="row">
+                <div className="col-sm">
+                    <Card {...this.state.form}/>
+                </div>
+                <div className="col-sm">
+                    <ExerciseForm
                         onChange={this.handleChange}
-                        value={this.state.title}
-                    />
+                        form={this.state.form}
+                    />            
                 </div>
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="description" 
-                        name="description"
-                        onChange={this.handleChange}
-                        value={this.state.description}
-                    />
-                </div>
-                <div className="form-group">
-                    <input 
-                        type="text" 
-                        className="form-control" 
-                        placeholder="img" 
-                        name="img"
-                        onChange={this.handleChange}
-                        value={this.state.img}
-                    />
-                </div>
-                <div className="form-row">
-                    <div className="col">
-                        <input 
-                            type="text" 
-                            className="form-control" 
-                            placeholder="leftColor" 
-                            name="leftColor"
-                            onChange={this.handleChange}
-                            value={this.state.leftColor}
-                        />
-                    </div>
-                    <div className="col">
-                        <input 
-                            type="text" 
-                            className="form-control"
-                            placeholder="rightColor" 
-                            name="rightColor"
-                            onChange={this.handleChange}
-                            value={this.state.rightColor}
-                        />    
-                    </div>
-                </div>
-                
-                <button 
-                    type="submit" 
-                    className="btn btn-primary"
-                >
-                    Submit
-                </button>
-            </form>
-        </div>
+            </div>
         )
     }
 }
