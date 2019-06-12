7b76dac9d24dca289411d13135e1e0af0233fc51
Raúl Palacios <raul@creatinkers.com>
2019-06-11 23:38:32 -0500

    migrate ExerciseNewContainer ClassComponent to FunctionalComponent with state hook

--- src/pages/ExerciseNewContainer.js
+++ src/pages/ExerciseNewContainer.js
@@ -1,37 +1,28 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../components/styles/ExerciseNew.css'
 import Loading from '../components/Loading'
 import FatalError from './500'
 import ExerciseNew from './ExerciseNew'
 
-class ExerciseNewContainer extends React.Component {
+const ExerciseNewContainer = ({history}) => {
+    const [ form, setForm ] = useState({title: '',
+                                description: '',
+                                img: '',
+                                leftColor: '',
+                                rightColor: ''
+                            })
+    const [ loading, setLoading ] = useState(false)
+    const [ error, setError ] = useState(null)
 
-    state = {
-        form: {
-            title: '',
-            description: '',
-            img: '',
-            leftColor: '',
-            rightColor: ''
-        },
-        loading: false,
-        error: null
-    }
-
-    handleChange = e => {
-        this.setState({
-            form: {
-                ...this.state.form,
-                [e.target.name]: e.target.value
-            }
+    const handleChange = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
         })
     }
 
-    handleSubmit = async e => {
-        this.setState({
-            loading: true
-        })
-
+    const handleSubmit = async e => {
+        setLoading(true)
         e.preventDefault()
         try {
             let config = {
@@ -40,39 +31,28 @@ class ExerciseNewContainer extends React.Component {
                     'Accept': 'application/json',
                     'Content-Type': 'application/json'
                 },
-                body: JSON.stringify(this.state.form)
+                body: JSON.stringify(form)
             }
-
-            let res = await fetch('http://localhost:8000/api/exercises', config)
-            let json = await res.json()
-
-            this.setState({
-                loading: false
-            })
-
-            this.props.history.push('/exercise')
+            await fetch('http://localhost:8000/api/exercises', config)
+            setLoading(false)
+            history.push('/exercise')
         } catch (error) {
-            this.setState({
-                loading: false,
-                error
-            })            
+            setLoading(false)
+            setError(error)
         }
-        
     }
 
-    render(){
-        if(this.state.loading)
-            return <Loading />
+    if(loading)
+        return <Loading />
 
-        if(this.state.error)
-            return <FatalError />
-            
-        return <ExerciseNew 
-            form={this.state.form}
-            onChange={this.handleChange}
-            onSubmit={this.handleSubmit}
-        />
-    }
+    if(error)
+        return <FatalError />
+
+    return <ExerciseNew 
+        form={form}
+        onChange={handleChange}
+        onSubmit={handleSubmit}
+    />
 }
 
 export default ExerciseNewContainer
